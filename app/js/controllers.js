a2cb862ee96a075d61b5ba4ed08afbb152c40257
Leo Lau <emailgag@gmail.com>
2013-06-08 17:36:10 +0800

    bind mails.json into view. yeah

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,47 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+// angular.module('myApp.controllers', []).
+//   controller('MailListCtrl', [function($scope, $http) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+// 	  $http.get('mails/mails.json').success(function(data) {
+	  	
+// 	  	console.log("success. old module here");
 
-  }]);
+// 	  	$scope.mails = data;
+
+// 	  });
+
+// 	  $scope.orderProp = 'dateReceived';
+
+//   }])
+//   .controller('MyCtrl2', [function() {
+
+//   	console.log("did you come to myctrl2?");
+
+//   }]);
+
+
+
+/* Controllers */
+
+function MailListCtrl($scope, $http) {
+  $http.get('mails/mails.json').success(function(data) {
+
+  	console.log("success. new function function here");
+
+  	$scope.mails = data;
+  });
+
+  $scope.orderProp = 'dateReceived';
+}
+
+//PhoneListCtrl.$inject = ['$scope', 'Phone'];
+
+function MyCtrl2($scope) {
+  $scope.state = "view2";
+}
+
+//PhoneListCtrl.$inject = ['$scope', 'Phone'];
+
+
