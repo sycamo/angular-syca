039893204450129ebaa63960e0b809fa86f3a878
Leo Lau <emailgag@gmail.com>
2013-06-09 23:05:28 +0800

    done with the ngservice, filters

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,60 +1,11 @@
 'use strict';
 
-/* Controllers */
-
-// angular.module('myApp.controllers', []).
-//   controller('MailListCtrl', [function($scope, $http) {
-
-// 	  $http.get('mails/mails.json').success(function(data) {
-	  	
-// 	  	console.log("success. old module here");
-
-// 	  	$scope.mails = data;
-
-// 	  });
-
-// 	  $scope.orderProp = 'dateReceived';
-
-//   }])
-//   .controller('MyCtrl2', [function() {
-
-//   	console.log("did you come to myctrl2?");
-
-//   }]);
-
-
-
-/* Controllers */
-
-// function MailListCtrl($scope, $http) {
-//   $http.get('mails/mails.json').success(function(data) {
-
-//   	console.log("success. new function function here");
-
-//   	$scope.mails = data;
-//   });
-
-//   $scope.orderProp = 'dateReceived';
-// }
-
-//PhoneListCtrl.$inject = ['$scope', 'Phone'];
-
-function MyCtrl2($scope) {
-  $scope.state = "view2";
-}
-
-//PhoneListCtrl.$inject = ['$scope', 'Phone'];
-
-
-
-/* ########################################## */
-
 
 /* Controllers */
 
 function MailListCtrl($scope, Mail) {
 	$scope.mails = Mail.query();
-  $scope.orderProp = 'dateReceived';
+  $scope.orderProp = '-dateReceived';
 }
 
 //MailListCtrl.$inject = ['$scope', 'Mail'];
