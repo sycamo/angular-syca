1ccb898363c4dfead81493da17dd10150f7d4f78
Leo Lau <emailgag@gmail.com>
2013-06-12 23:50:31 +0800

    succeeded adding custom truncate filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -9,26 +9,50 @@ angular.module('sycamoFilters', []).
     }
   }]);
 
-angular.module('myFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
-  };
-});
+angular.module('myFilters', []).
+	filter('checkmark', function() { // just a test filter
+	  return function(input) {
+	    return input ? '\u2713' : '\u2718';
+	  };
+	}). // the dot here connects the next filter function
+	
+	filter('truncate', function() {
+		return function (text, limit, end) {
+	        if (isNaN(limit))
+	            limit = 120;
 
-angular.module('myFilters2', []).filter('truncate', function() {
-	return function (text, limit, end) {
-        if (isNaN(limit))
-            limit = 10;
+	        if (end === undefined)
+	            end = "...";
 
-        if (end === undefined)
-            end = "...";
+	        if (text == null || text.length == 0) {
+        			return null;
+        	}
+	        else if (text.length <= limit || text.length - end.length <= limit) {
+	            return text;
+	        } 
+	        else {
+	            return String(text).substring(0, limit-end.length) + end;
+	        }
 
-        if (text.length <= limit || text.length - end.length <= limit) {
-            return text;
-        }
-        else {
-            return String(text).substring(0, limit-end.length) + end;
-        }
+	    };
 
-    };
-});
+	});
+
+// angular.module('myFilters2', []).
+// 	filter('truncate', function() {
+// 		return function (text, limit, end) {
+// 	        if (isNaN(limit))
+// 	            limit = 140;
+
+// 	        if (end === undefined)
+// 	            end = "...";
+
+// 	        if (text.length <= limit || text.length - end.length <= limit) {
+// 	            return text;
+// 	        }
+// 	        else {
+// 	            return String(text).substring(0, limit-end.length) + end;
+// 	        }
+
+// 	    };
+// });
